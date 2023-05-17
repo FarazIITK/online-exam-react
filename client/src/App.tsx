b2d744e5a215c13857249b2733d6137d1e27db1c
fhusain01 <fhusain@adaptready.com>
2023-05-17 20:26:33 +0530

    Added interface for API response and added state for questions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,38 @@
+import { useState } from 'react';
 import './App.css';
 import QuestionAndOptions from './Components/Question';
 
+export interface IQuestionData {
+  id: number;
+  answer: string;
+  question: string;
+  value: number;
+  airdate: string;
+  created_at: string;
+  updated_at: string;
+  category_id: number;
+  game_id: number;
+  invalid_count: number;
+  category: {
+    id: number;
+    title: string;
+    created_at: string;
+    updated_at: string;
+    clues_count: number;
+  };
+}
+
 function App() {
+  const [questionsData, setQuestionsData] = useState<
+    IQuestionData[] | null
+  >(null);
+
   return (
     <div className="App">
-      <QuestionAndOptions />
+      <QuestionAndOptions
+        questionsData={questionsData}
+        setQuestionsData={setQuestionsData}
+      />
     </div>
   );
 }
