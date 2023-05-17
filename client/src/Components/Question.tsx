68dc10a431010ce93240021a29155da6b9276366
fhusain01 <fhusain@adaptready.com>
2023-05-17 20:26:57 +0530

    Added logic to make API call to get questions

--- client/src/Components/Question.tsx
+++ client/src/Components/Question.tsx
@@ -1,4 +1,27 @@
-const QuestionAndOptions = () => {
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { IQuestionData } from '../App';
+
+interface IProp {
+  questionsData: IQuestionData[] | null;
+  setQuestionsData: React.Dispatch<
+    React.SetStateAction<IQuestionData[] | null>
+  >;
+}
+
+const QuestionAndOptions = (props: IProp) => {
+  const questionApiUrl = `https://jservice.io/api/random`;
+  const numberOfQuestions = 5;
+
+  useEffect(() => {
+    axios
+      .get(`${questionApiUrl}?count=${numberOfQuestions}`)
+      .then((response) => {
+        console.log('Data: ', response.data);
+        props.setQuestionsData(response.data);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Question</h1>
